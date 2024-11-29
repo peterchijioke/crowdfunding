0026738e0e18212ba8646d9e0a286bc3ff4ab87d
Chukwu (Chijioke)  Peter <peterchijioke1@gmail.com>
2024-11-29 13:33:56 +0100

    fix: set up

---
+++ programs/crowdfunding/src/lib.rs
@@ -0,0 +1,40 @@
+use anchor_lang::prelude::*;
+
+declare_id!("F7Knnyv4EZjxrVeW18xmygXXrWsSwpbhyuBiku8CGAaz");
+
+#[program]
+pub mod crowdfunding {
+    use super::*;
+
+    pub fn create(ctx: Context<Create>, name: String, description: String) -> Result<()> {
+        let campaign = &mut ctx.accounts.campaign;
+        campaign.name = name;
+        campaign.description = description;
+        campaign.amount_donated = 0;
+        campaign.admin = *ctx.accounts.user.key;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Create<'info> {
+    #[account(
+        init,
+        payer = user,
+        space = 9000,
+        seeds = [b"CAMPAIGN_DEMO", user.key().as_ref()],
+        bump
+    )]
+    pub campaign: Account<'info, Campaign>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Campaign {
+    pub admin: Pubkey,
+    pub name: String,
+    pub description: String,
+    pub amount_donated: u64,
+}
