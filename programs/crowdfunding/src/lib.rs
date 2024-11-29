a2797b13670a0796558e27ad6fb388f507d9343d
Chukwu (Chijioke)  Peter <peterchijioke1@gmail.com>
2024-11-29 14:27:17 +0100

    fix: added the withdraw function

--- programs/crowdfunding/src/lib.rs
+++ programs/crowdfunding/src/lib.rs
@@ -1,17 +1,37 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::entrypoint::ProgramResult;
 
 declare_id!("F7Knnyv4EZjxrVeW18xmygXXrWsSwpbhyuBiku8CGAaz");
 
 #[program]
 pub mod crowdfunding {
+    use anchor_lang::solana_program::entrypoint::ProgramResult;
+
     use super::*;
 
-    pub fn create(ctx: Context<Create>, name: String, description: String) -> Result<()> {
+    pub fn create(ctx: Context<Create>, name: String, description: String) -> ProgramResult {
         let campaign = &mut ctx.accounts.campaign;
         campaign.name = name;
         campaign.description = description;
         campaign.amount_donated = 0;
         campaign.admin = *ctx.accounts.user.key;
+        Ok(())
+    }
+    pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> ProgramResult {
+        let campaign = &mut ctx.accounts.campaign;
+        let user = &mut ctx.accounts.user;
+
+        if campaign.admin != *user.key {
+            return Err(ProgramError::IncorrectProgramId.into());
+        }
+
+        let rent_balance = Rent::get()?.minimum_balance(campaign.to_account_info().data_len());
+
+        if **campaign.to_account_info().lamports.borrow() - rent_balance < amount {
+            return Err(ProgramError::InsufficientFunds);
+        }
+        **campaign.to_account_info().try_borrow_mut_lamports()? -= amount;
+        **user.to_account_info().try_borrow_mut_lamports()? += amount;
         Ok(())
     }
 }
@@ -37,4 +57,13 @@ pub struct Campaign {
     pub name: String,
     pub description: String,
     pub amount_donated: u64,
+}
+
+#[derive(Accounts)]
+pub struct Withdraw<'info> {
+    #[account(mut)]
+    pub campaign: Account<'info, Campaign>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
 }
